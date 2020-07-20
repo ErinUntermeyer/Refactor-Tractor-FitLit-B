83bb80d3c07a1827b3c89b6f01f55a629b0eb26b
Nick Hart <nick.hart.des@gmail.com>
2020-07-20 14:47:35 -0600

    Clean up existing user repo tests #22

--- test/userrepo-test.js
+++ test/userrepo-test.js
@@ -38,36 +38,17 @@ describe('User Repo', function() {
   });
 
   it('should have a parameter to take in user data', function() {
-    const user1 = new User({
-      id: 1,
-      name: "Alex Roth",
-      address: "1234 Turing Street, Denver CO 80301-1697",
-      email: "alex.roth1@hotmail.com",
-      strideLength: 4.3,
-      dailyStepGoal: 10000,
-      friends: [2, 3, 4]
-    });
     const users = [user1];
     const userRepo = new UserRepo(users);
-
-    console.log('here: ', userRepo.users[0]);
 
     expect(userRepo.users[0].id).to.equal(1);
   });
 
   it('should return user data when given user ID', function() {
-
-
-    userRepo.getDataFromID(1);
-
     expect(userRepo.getDataFromID(1)).to.eql(user1);
   });
 
   it('should return the average of all users step goals', function() {
-
-
-    userRepo.calculateAverageStepGoal();
-
     expect(userRepo.calculateAverageStepGoal()).to.eql(9500);
   });
  });
