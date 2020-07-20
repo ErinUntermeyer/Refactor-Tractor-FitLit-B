04febe41a7acd41416771a4010045ebf3543ecb3
Nick Hart <nick.hart.des@gmail.com>
2020-07-20 14:23:50 -0600

    Fix formatting/style conventions #21

--- src/User.js
+++ src/User.js
@@ -1,12 +1,12 @@
 class User {
   constructor(userData) {
-    this.id = this.checkDataType(userData.id, "number");
-    this.name = this.checkDataType(userData.name, "string");
-    this.address = this.checkDataType(userData.address, "string");
-    this.email = this.checkDataType(userData.email, "string");
-		this.strideLength = this.checkDataType(userData.strideLength, "number");
-		this.dailyStepGoal = this.checkDataType(userData.dailyStepGoal, "number");
-    this.friends = this.checkDataType(userData.friends, "object");
+    this.id = this.checkDataType(userData.id, 'number');
+    this.name = this.checkDataType(userData.name, 'string');
+    this.address = this.checkDataType(userData.address, 'string');
+    this.email = this.checkDataType(userData.email, 'string');
+		this.strideLength = this.checkDataType(userData.strideLength, 'number');
+		this.dailyStepGoal = this.checkDataType(userData.dailyStepGoal, 'number');
+    this.friends = this.checkDataType(userData.friends, 'object');
 	}
 	
 	checkDataType(attribute, dataType) {
--- test/user-test.js
+++ test/user-test.js
@@ -5,50 +5,51 @@ import User from '../src/User';
 
 describe('User', function() {
 	let user1, user2, user3, user4, badUser;
+	
 	beforeEach(function() {
 		user1 = new User({
 			id: 1,
-			name: "Alex Roth",
-			address: "1234 Turing Street, Denver CO 80301-1697",
-			email: "alex.roth1@hotmail.com",
+			name: 'Alex Roth',
+			address: '1234 Turing Street, Denver CO 80301-1697',
+			email: 'alex.roth1@hotmail.com',
 			strideLength: 4.3,
 			dailyStepGoal: 10000,
 			friends: [2, 3, 4]
 		}),
 		user2 = new User({
 			id: 2,
-			name: "Allie McCarthy",
-			address: "1235 Turing Street, Denver CO 80301-1697",
-			email: "allie.mcc1@hotmail.com",
+			name: 'Allie McCarthy',
+			address: '1235 Turing Street, Denver CO 80301-1697',
+			email: 'allie.mcc1@hotmail.com',
 			strideLength: 3.3,
 			dailyStepGoal: 9000,
 			friends: [1, 3, 4]
 		}),
 		user3 = new User({
 			id: 3,
-			name: "The Rock",
-			address: "1236 Awesome Street, Denver CO 80301-1697",
-			email: "therock@hotmail.com",
+			name: 'The Rock',
+			address: '1236 Awesome Street, Denver CO 80301-1697',
+			email: 'therock@hotmail.com',
 			strideLength: 10,
 			dailyStepGoal: 60000,
 			friends: [1, 2, 4]
 		}),
 		user4 = new User({
 			id: 4,
-			name: "Rainbow Dash",
-			address: "1237 Equestria Street, Denver CO 80301-1697",
-			email: "rainbowD1@hotmail.com",
+			name: 'Rainbow Dash',
+			address: '1237 Equestria Street, Denver CO 80301-1697',
+			email: 'rainbowD1@hotmail.com',
 			strideLength: 3.8,
 			dailyStepGoal: 7000,
 			friends: [1, 2, 3]
 		}),
 		badUser = new User({
-			id: "one",
-			name: ["Bad User"],
-			address: {address: "1234 Turing Street, Denver CO 80301-1697"},
-			email: {email: "baduser@hotmail.com"},
-			strideLength: "4.3",
-			dailyStepGoal: "a lot",
+			id: 'one',
+			name: ['Bad User'],
+			address: {address: '1234 Turing Street, Denver CO 80301-1697'},
+			email: {email: 'baduser@hotmail.com'},
+			strideLength: '4.3',
+			dailyStepGoal: 'a lot',
 			friends: 2
 		});
 	});
@@ -63,9 +64,9 @@ describe('User', function() {
 
   it.only('should take info from user data', function() {
     expect(user1.id).to.equal(1);
-		expect(user1.name).to.equal("Alex Roth");
-		expect(user1.address).to.equal("1234 Turing Street, Denver CO 80301-1697");
-		expect(user1.email).to.equal("alex.roth1@hotmail.com");
+		expect(user1.name).to.equal('Alex Roth');
+		expect(user1.address).to.equal('1234 Turing Street, Denver CO 80301-1697');
+		expect(user1.email).to.equal('alex.roth1@hotmail.com');
 		expect(user1.strideLength).to.equal(4.3);
 		expect(user1.dailyStepGoal).to.equal(10000);
 		expect(user1.friends).to.deep.equal([2, 3, 4]);
@@ -88,7 +89,7 @@ describe('User', function() {
 	});
 	
   it.only('should return user first name', function() {
-    expect(user2.getFirstName()).to.equal("Allie");
+    expect(user2.getFirstName()).to.equal('Allie');
   });
 
   it.only('should return list of friend names from user repository', function() {
