82c439e3f3d1cfba1dc89b493b2ef4de2c79497e
Kyle Jones <rmbart395@yahoo.com>
2020-07-22 15:36:24 -0600

    Add checkDataType method to Data class and all testing required for the class #37

--- src/Data.js
+++ src/Data.js
@@ -1,9 +1,11 @@
 class Data {
   constructor(userID, date) {
-    this.userID = userID;
-    this.date = date;
-
+    this.userID = this.checkDataType(userID, 'number');
+    this.date = this.checkDataType(date, "string");
   }
+  checkDataType(attribute, dataType) {
+		return typeof attribute === dataType ? attribute : null;
+	};
 }
 
 
--- test/data-test.js
+++ test/data-test.js
@@ -3,12 +3,34 @@ import { expect } from 'chai';
 import Data from '../src/Data';
 
 describe('Data', function() {
+  let data, badData;
+
+  beforeEach(function() {
+    data = new Data(1, "07/22/2020");
+    badData = new Data("I dont believe in numbers", true)
+  })
 
   it.only('should be a function', function() {
     expect(Data).to.be.a('function');
   });
 
-  it.skip('should be an instance of Data', function() {
+  it.only('should be an instance of Data', function() {
     expect(data).to.be.an.instanceof(Data);
+  });
+
+  it.only('should have a userID', function() {
+    expect(data.userID).to.equal(1);
+  });
+
+  it.only('should have a date', function() {
+    expect(data.date).to.equal("07/22/2020");
+  });
+
+  it.only('should only accept a number for userID', function() {
+    expect(badData.userID).to.equal(null);
+  });
+
+  it.only('should only accept a string for date', function() {
+    expect(badData.date).to.equal(null);
   });
 })
