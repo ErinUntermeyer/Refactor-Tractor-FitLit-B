d68b2bfc4f10e1270598c9f41dc0a53762cafd43
Kyle Jones <rmbart395@yahoo.com>
2020-07-23 14:34:57 -0600

    Add code to refactor the hydration.js and hydration-test.js #23

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,30 +1,11 @@
-class Hydration {
+class Hydration extends Data {
   constructor(hydrationData) {
+    super();
     this.userID = hydrationData.userID;
     this.date = hydrationData.date;
     this.numOunces = hydrationData.numOunces;
-  }
-
-  calculateAverageOunces(id) {
-    let perDayUserHydration = this.hydrationData.filter((data) => id === data.userID);
-    return perDayUserHydration.reduce((sumSoFar, data) => {
-      return sumSoFar += data.numOunces;
-    }, 0) / perDayUserHydration.length;
-  }
-
-  calculateDailyOunces(id, date) {
-    let findOuncesByDate = this.hydrationData.find((data) => id === data.userID && date === data.date);
-    return findOuncesByDate.numOunces;
-  }
-
-  calculateFirstWeekOunces(userRepo, id) {
-    return userRepo.getFirstWeek(id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
-  
-  calculateRandomWeekOunces(date, id, userRepo) {
-    return userRepo.getWeekFromDate(date, id, this.hydrationData).map((data) => `${data.date}: ${data.numOunces}`);
-  }
-}
+  };
+};
 
 
 export default Hydration;
