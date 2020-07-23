17bab67250c122e57299e163bf8c97c5e654937d
Kyle Jones <rmbart395@yahoo.com>
2020-07-23 16:25:38 -0600

    Add code to refactor Activity.js and activity-test.js #24

--- src/Activity.js
+++ src/Activity.js
@@ -1,71 +1,14 @@
-class Activity {
+import Data from './Data';
+
+class Activity extends Data {
   constructor(activityData) {
-    this.userID = activityData.userID;
-    this.date = activityData.date;
-    this.numSteps = activityData.numSteps;
-    this.minutesActive = activityData.minutesActive;
-    this.flightsOfStairs = activityData.flightsOfStairs;
-  }
-
-  getAllUserAverageForDay(date, userRepo, relevantData) {
-    let selectedDayData = userRepo.chooseDayDataForAllUsers(this.activityData, date);
-    return parseFloat((selectedDayData.reduce((acc, elem) => acc += elem[relevantData], 0) / selectedDayData.length).toFixed(1));
-  }
-  userDataForToday(id, date, userRepo, relevantData) {
-    let userData = userRepo.getDataFromUserID(id, this.activityData);
-    return userData.find(data => data.date === date)[relevantData];
-  }
-  userDataForWeek(id, date, userRepo, releventData) {
-    return userRepo.getWeekFromDate(date, id, this.activityData).map((data) => `${data.date}: ${data[releventData]}`);
-  }
-
-  getFriendsActivity(user, userRepo) {
-    let data = this.activityData;
-    let userDatalist = user.friends.map(function(friend) {
-      return userRepo.getDataFromUserID(friend, data)
-    });
-    return userDatalist.reduce(function(arraySoFar, listItem) {
-      return arraySoFar.concat(listItem);
-    }, []);
-  }
-  getFriendsAverageStepsForWeek(user, date, userRepo) {
-    let friendsActivity = this.getFriendsActivity(user, userRepo);
-    let timeline = userRepo.chooseWeekDataForAllUsers(friendsActivity, date);
-    return userRepo.combineRankedUserIDsAndAveragedData(friendsActivity, date, 'numSteps', timeline)
-  }
-  showChallengeListAndWinner(user, date, userRepo) {
-    let rankedList = this.getFriendsAverageStepsForWeek(user, date, userRepo);
-
-    return rankedList.map(function(listItem) {
-      let userID = Object.keys(listItem)[0];
-      let userName = userRepo.getDataFromID(parseInt(userID)).name;
-      return `${userName}: ${listItem[userID]}`
-    })
-  }
-  showcaseWinner(user, date, userRepo) {
-    let namedList = this.showChallengeListAndWinner(user, date, userRepo);
-    let winner = this.showChallengeListAndWinner(user, date, userRepo).shift();
-    return winner;
-  }
-  getStreak(userRepo, id, relevantData) {
-    let data = this.activityData;
-    let sortedUserArray = (userRepo.makeSortedUserArray(id, data)).reverse();
-    let streaks = sortedUserArray.filter(function(element, index) {
-      if (index >= 2) {
-        return (sortedUserArray[index - 2][relevantData] < sortedUserArray[index - 1][relevantData] && sortedUserArray[index - 1][relevantData] < sortedUserArray[index][relevantData])
-      }
-    });
-    return streaks.map(function(streak) {
-      return streak.date;
-    })
-  }
-  getWinnerId(user, date, userRepo) {
-    let rankedList = this.getFriendsAverageStepsForWeek(user, date, userRepo);
-    let keysList = rankedList.map(listItem => Object.keys(listItem));
-    return parseInt(keysList[0].join(''))
-  }
-}
-
-
+    super()
+    this.userID = super.checkDataType(activityData.userID, 'number');
+    this.date = super.checkDataType(activityData.date, 'string');
+    this.numSteps = super.checkDataType(activityData.numSteps, 'number');
+    this.minutesActive = super.checkDataType(activityData.minutesActive, 'number');
+    this.flightsOfStairs = super.checkDataType(activityData.flightsOfStairs, 'number');
+  };
+};
 
 export default Activity;
